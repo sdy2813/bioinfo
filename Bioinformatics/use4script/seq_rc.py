a6ad8b7d1f337a3ce986dd30ebc40762a32bc204
Xum <xum2813@gmail.com>
2022-03-10 21:45:25 +0800

    Update seq_rc.py

--- Bioinformatics/use4script/seq_rc.py
+++ Bioinformatics/use4script/seq_rc.py
@@ -9,14 +9,14 @@ Created on Thu Mar 10 19:52:58 2022
 from Bio.Seq import Seq
 import argparse
 
-parser = argparse.ArgumentParser(add_help = False, usage = '\npython3 seq_rc.py -i [fa] -o [fa]\n用于')
+parser = argparse.ArgumentParser(add_help = False, usage = '\npython3 seq_rc.py -i [fa] -o [fa]\n用于获取反向序列和反向互补序列')
 required = parser.add_argument_group()
 optional = parser.add_argument_group()
-required.add_argument('-i', '--input', metavar = '[fa]', help = '输入文件，fa 文件', required = True)
-required.add_argument('-o', '--output', metavar = '[fa]', help = '输出文件，fa 文件', required = True)
-optional.add_argument('-c', '--complement',  action = "store_true")
-optional.add_argument('-rc', '--reverse_complement',  action = "store_true")
-optional.add_argument('-h', '--help', action = 'help', help = '帮助信息')
+required.add_argument('-i'， '--input'， metavar = '[fa]'， help = '输入文件，fa 文件'， required = True)
+required.add_argument('-o'， '--output'， metavar = '[fa]'， help = '输出文件，fa 文件'， required = True)
+optional.add_argument('-c'， '--complement'， help = '互补序列'， action = "store_true")
+optional.add_argument('-rc'， '--reverse_complement'， help = '反向互补序列'， action = "store_true")
+optional.add_argument('-h'， '--help', action = 'help'， help = '帮助信息')
 
 args = parser.parse_args()
 seq=open(args.input,"r")
